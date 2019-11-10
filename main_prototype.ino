b9300f1a43fe28139608979814daa09f49dca4b8
egor_follia <egorfollia@gmail.com>
2019-11-10 20:21:25 +0300

    Переделал вариант кода, когда будет новый драйвер. Упростил структуру

--- main_prototype.ino
+++ main_prototype.ino
@@ -1,37 +1,31 @@
 #include <Wire.h>
-#include <TroykaIMU.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+//#include <TroykaIMU.h>
 #include <WiFiClient.h>
-#include <WiFiUdp.h>                      // changed from UDP to Udp for linux-distros
+#include "WEMOS_Motor.h"
 
-// множитель фильтра
-#define BETA 0.22f
-
-// STATE MACHINE
-//#define ST_INIT 1
-//#define ST_WAIT_CONNECT 2
-//#define ST_IDLE 3
-//#define ST_MOVE 4
-//#define ST_STOP 5
-enum state_machine { ST_INIT, ST_WAIT_CONNECT, ST_IDLE, ST_MOVE, ST_STOP };
-
-int DV1_1 = 12;
-int DV1_2 = 14;
-int DV2_1 = 15;
-int DV2_2 = 13;
-
+// ------ UDP and AP ------ //
 const char* ssid = "ROBOLEAGUE";
 const char* password = "1234567890";
 
 WiFiUDP Udp;
 unsigned int UdpPort = 4210;                    // local port to listen on
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];      // buffer to hold incoming packet
+char packetBuffer[255];      // buffer to hold incoming packet
 char  ReplyBuffer[] = "pososi, potom prosi";    // a string to send backchar
 
-Madgwick filter;     // создаём объект для фильтра Madgwick
-Accelerometer accel; // создаём объект для работы с акселерометром
-Gyroscope gyro;      // создаём объект для работы с гироскопом
+// ------ ---------- ------ //
 
+// STATE MACHINE
+enum state_machine { ST_INIT, ST_WAIT_CONNECT, ST_IDLE, ST_MOVE, ST_STOP };
+
+// множитель фильтра
+#define BETA 0.22f
+
+//Madgwick filter;     // создаём объект для фильтра Madgwick
+//Accelerometer accel; // создаём объект для работы с акселерометром
+//Gyroscope gyro;      // создаём объект для работы с гироскопом
+//
 float gx, gy, gz, ax, ay, az;   // переменные для данных с гироскопов, акселерометров
 float yaw, pitch, roll;         // получаемые углы ориентации
 float fps = 100;                // переменная для хранения частоты выборок фильтра
@@ -39,73 +33,20 @@ bool reversed = false;          // флаг состояния машинки
 int leftSpeed = 0;
 int rightSpeed = 0;
 
-class Motor                     // класс для работы с ДПТ, подключенными через драйвер
+void moveVehicle(int speedy, bool inverted, Motor M1, Motor M2) 
 {
-  public:
-    int pin1; //пин для задания направления или ШИМ
-    int pin2; //пин для задания направления или ШИМ//пин для задания направления или ШИМ
-  public:
-    Motor(int p1, int p2): // ну конструктор, ну ясно жи
-      pin1(p1),
-      pin2(p2)
-    {
-    }
-    void initilize() //инициализация пинов, к которым подключен двигатель
-    {
-      pinMode(pin1, OUTPUT);
-      pinMode(pin2, OUTPUT);
-    }
-    void moveConst(int speedy, bool inverted) // постоянное вращение с учетом перевернутости машинки
-    {
-      // машина состояний, отрабатывающая разные ситуации (перевернутость машинки и направление вектора скорости)
-      if (inverted == false && speedy > 0)
-      {
-        digitalWrite(pin1, LOW);
-        analogWrite(pin2, speedy);
-      }
-      else if (inverted == false && speedy < 0)
-      {
-        digitalWrite(pin1, speedy);
-        analogWrite(pin2, LOW);
-      }
-      else if (inverted == true && speedy > 0)
-      {
-        digitalWrite(pin1, speedy);
-        analogWrite(pin2, LOW);
-      }
-      else if (inverted == true && speedy < 0)
-      {
-        digitalWrite(pin1, LOW);
-        analogWrite(pin2, speedy);
-      }
-      else if (speedy == 0)
-      {
-        digitalWrite(pin1, LOW);
-        analogWrite(pin2, LOW);
-      }
-    }
-    void moveConst(int speedy) // постоянное вращение без учета перевернутости машинки
-    {
-      if (speedy > 0)
-      {
-        digitalWrite(pin1, LOW);
-        analogWrite(pin2, speedy);
-      }
-      if (speedy < 0)
-      {
-        Serial.println(pin1);
-        digitalWrite(pin1, speedy);
-        analogWrite(pin2, LOW);
-      }
-    }
-};
-Motor leftMotor(DV1_1, DV1_2);
-Motor rightMotor(DV2_1, DV2_2);
+  // машина состояний, отрабатывающая разные ситуации (перевернутость машинки и направление вектора скорости)
+  if (inverted == false)
+  {
+    M1.setmotor(_CW, speedy);
+    M2.setmotor(_CW, speedy);
+  }
 
-void moveVehicle(int l, int r, bool inv)
-{
-  leftMotor.moveConst(l, inv);
-  rightMotor.moveConst(r, inv);
+  else if (inverted == true)
+  {
+    M1.setmotor(_CCW, speedy);
+    M2.setmotor(_CCW, speedy);
+  }
 }
 
 void waitConnection()
@@ -118,8 +59,11 @@ void waitStart()
   // ST IDLE block code
 }
 
+Motor motorA(0x30, _MOTOR_A, 1000);
+Motor motorB(0x30, _MOTOR_B, 1000);
 uint8_t state;
-void setup() 
+
+void setup()
 {
   // открываем последовательный порт
   Serial.begin(115200);
@@ -129,29 +73,19 @@ void setup()
   delay(5);
   Udp.begin(UdpPort);
   delay(5);
-//  leftMotor.initilize();
-//  rightMotor.initilize();
 
-  accel.begin();  // инициализация акселерометра
-  gyro.begin();   // инициализация гироскопа
-
-  int state = ST_INIT;
+  // Добавить функции с возвращающими значениями ( if return true -> init sucess )
+//  accel.begin();  // инициализация акселерометра
+//  gyro.begin();   // инициализация гироскопа
+  Serial.println("Initialization completed"); // выводим сообщение об удачной инициализации
 }
 
-void loop() 
+void loop()
 {
   while (true)
   {
     switch(state)
     {
-      case ST_INIT:
-        leftMotor.initilize();
-        rightMotor.initilize();
-        Serial.println("Initialization completed"); // выводим сообщение об удачной инициализации
-
-        // Добавить проверку на инициализацию
-        
-        break;
       case ST_WAIT_CONNECT:
         waitConnection(); // Ожидания соединения
         break;
@@ -159,10 +93,64 @@ void loop()
         waitStart();
         break;
       case ST_MOVE:
-        // Проверка данных с IMU, настройка флагов и скоростей, считывание данных и запуск
+        unsigned long startMillis = millis(); // запоминаем текущее время// запоминаем текущее время
+//        accel.readGXYZ(&ax, &ay, &az); // считываем данные с акселерометра в единицах G
+//        gyro.readRadPerSecXYZ(&gx, &gy, &gz); // считываем данные с акселерометра в радианах в секунду
+//
+//        filter.setKoeff(fps, BETA); // устанавливаем коэффициенты фильтра
+//        filter.update(gx, gy, gz, ax, ay, az); // обновляем входные данные в фильтр
+
+//        roll = filter.getRollDeg(); // получение углов yaw, pitch и roll из фильтра
+
+        //Serial.print("roll: "); // выводим полученные углы в serial-порт
+        //Serial.println(roll);
+
+        unsigned long deltaMillis = millis() - startMillis; // вычисляем затраченное время на обработку данных
+
+        fps = 1000 / deltaMillis; // вычисляем частоту обработки фильтра
+
+        if (roll < 150.0 && roll > -150.0) // проверяем перевернута ли машинка
+          reversed = false;
+        else
+          reversed = true;
+
+        int packetSize = Udp.parsePacket();
+
+        if (packetSize)
+        {
+          // read the packet into packetBufffer
+          int len = Udp.read(packetBuffer, 255);
+          if (len > 0)
+            packetBuffer[len] = 0;
+          //Serial.println("Contents:");
+          //Serial.println(packetBuffer);
+
+          String str(packetBuffer);
+          String lstr, rstr;
+          str = str.substring(0, str.indexOf(';'));
+          lstr = str.substring(0, str.indexOf(','));
+          rstr = str.substring(str.indexOf(',') + 1, str.indexOf(';'));
+
+          leftSpeed = lstr.toInt();
+          rightSpeed = rstr.toInt();
+
+          Serial.print(leftSpeed);
+          Serial.print(" ");
+          Serial.println(rightSpeed);
+
+          //Udp.flush();
+          // send a reply, to the IP address and port that sent us the packet we received
+          //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+          //Udp.write(ReplyBuffer);
+          //Udp.endPacket();
+        }
+
+        moveVehicle(leftSpeed, reversed, motorA, motorB);
+        
         break;
-      case ST_STOP:
-        break;
+//      case ST_STOP:
+//        Serial.print("Stop");
+//        break;
     }
   }
 
