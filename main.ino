2c345abeef8933bc23d17d3e950c220bbfd7e610
egor_follia <egorfollia@gmail.com>
2019-11-18 17:50:05 +0300

    Новый класс, пока без IMU. Тестировано и работает

--- main.ino
+++ main.ino
@@ -1,124 +1,203 @@
-#include <Robot_football.h>
+#include <Wire.h>
+#include <TroykaIMU.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUDP.h>
 
-// ---------- Declare all objects
-WiFiUDP Udp;                            // UDP conenction
-Motor motorA(0x30, _MOTOR_A, 1000);     // Motors left and right side
-Motor motorB(0x30, _MOTOR_B, 1000);
-// -------------------- //
+// множитель фильтра
+#define BETA 0.22f
 
-// ---------- State machine
-enum state_machine { ST_INIT, ST_CONNECT, ST_IDLE, ST_MOVE, ST_STOP };
-uint8_t state;
-// -------------------- //
+const char* ssid = "ROBOLEAGUE";
+const char* password = "1234567890";
 
-// ---------- Class robot
-class Robot
+WiFiUDP Udp;
+unsigned int UdpPort = 4210;  // local port to listen on
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "pososi, potom prosi";       // a string to send backchar
+
+Madgwick filter; // создаём объект для фильтра Madgwick
+
+Accelerometer accel; // создаём объект для работы с акселерометром
+Gyroscope gyro; // создаём объект для работы с гироскопом
+
+float gx, gy, gz, ax, ay, az; // переменные для данных с гироскопов, акселерометров
+
+float yaw, pitch, roll; // получаемые углы ориентации
+
+float fps = 100; // переменная для хранения частоты выборок фильтра
+
+bool reversed = false; // флаг состояния машинки
+
+int leftSpeed = 0;
+int rightSpeed = 0;
+
+class Motor // класс для работы с ДПТ, подключенными через драйвер
 {
-public:
-  const char* m_ssid = "ROBOLEAGUE";
-  const char* m_password = "1234567890";
-  unsigned int m_UdpPort = 4210;                      // local port to listen on
-  char m_packetBuffer[255];                           // buffer to hold incoming packet
-
-  bool m_carPosition = false;                            // vehicle position
-  int m_speedy = 0;                                   // initial speed
-
-public:
-  void initialize()
-  {
-    state = ST_INIT;
-  }
-
-  void waitConnection()
-  {
-    Serial.println(WiFi.softAP(m_ssid, m_password) ? "AP connected" : "Failed to connect!");
-    Udp.begin(m_UdpPort);
-
-    state = ST_CONNECT;
-  }
-
-  void idleVehicle()
-  {
-    state = ST_IDLE;
-
-    motorA.setmotor(_STANDBY);
-    motorB.setmotor(_STANDBY);
-  }
-
-  void moveVehicle()
-  {
-    state = ST_MOVE;
-
-    if (m_carPosition)
+  public:
+    int pin1; //пин для задания направления или ШИМ
+    int pin2; //пин для задания направления или ШИМ//пин для задания направления или ШИМ
+  public:
+    Motor(int p1, int p2): // ну конструктор, ну ясно жи
+      pin1(p1),
+      pin2(p2)
     {
-      motorA.setmotor(_CW, m_speedy);
-      motorB.setmotor(_CCW, m_speedy);
     }
-
-    else
+    void initilize() //инициализация пинов, к которым подключен двигатель
     {
-      motorA.setmotor(_CCW, m_speedy);
-      motorB.setmotor(_CW, m_speedy);
+      pinMode(pin1, OUTPUT);
+      pinMode(pin2, OUTPUT);
     }
-  }
-
-  void stopVehicle()
-  {
-    state = ST_STOP;
-
-    motorA.setmotor(_STOP);
-    motorB.setmotor(_STOP);
-  }
+    void moveConst(int speedy, bool inverted) // постоянное вращение с учетом перевернутости машинки
+    {
+      // машина состояний, отрабатывающая разные ситуации (перевернутость машинки и направление вектора скорости)
+      if (inverted == false && speedy > 0)
+      {
+        digitalWrite(pin1, LOW);
+        analogWrite(pin2, speedy);
+      }
+      else if (inverted == false && speedy < 0)
+      {
+        digitalWrite(pin1, speedy);
+        analogWrite(pin2, LOW);
+      }
+      else if (inverted == true && speedy > 0)
+      {
+        digitalWrite(pin1, speedy);
+        analogWrite(pin2, LOW);
+      }
+      else if (inverted == true && speedy < 0)
+      {
+        digitalWrite(pin1, LOW);
+        analogWrite(pin2, speedy);
+      }
+      else if (speedy == 0)
+      {
+        digitalWrite(pin1, LOW);
+        analogWrite(pin2, LOW);
+      }
+    }
+    void moveConst(int speedy) // постоянное вращение без учета перевернутости машинки
+    {
+      if (speedy > 0)
+      {
+        digitalWrite(pin1, LOW);
+        analogWrite(pin2, speedy);
+      }
+      if (speedy < 0)
+      {
+        Serial.println(pin1);
+        digitalWrite(pin1, speedy);
+        analogWrite(pin2, LOW);
+      }
+    }
 };
-// -------------------- //
 
-Robot car;
+int DV1_1 = 12;
+int DV1_2 = 14;
+int DV2_1 = 15;
+int DV2_2 = 13;
+
+Motor leftMotor(DV1_1, DV1_2);
+Motor rightMotor(DV2_1, DV2_2);
+
+
+void moveVehicle(int l, int r, bool inv)
+{
+  leftMotor.moveConst(l, inv);
+  rightMotor.moveConst(r, inv);
+}
+
 void setup()
 {
+  // открываем последовательный порт
   Serial.begin(115200);
-  Serial.println("Begin initialization ...");
+  Serial.println("Begin init...");
+  delay(5);
+  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
+  delay(5);
+  Udp.begin(UdpPort);
+  delay(5);
+  leftMotor.initilize();
+  rightMotor.initilize();
 
-  delay(5);
-  car.waitConnection();
-  delay(5);
+  accel.begin();  // инициализация акселерометра
+  gyro.begin(); // инициализация гироскопа
 
   Serial.println("Initialization completed"); // выводим сообщение об удачной инициализации
 }
 
 void loop()
 {
-  switch (state)
-  {
-    case ST_INIT:
-      car.initialize();
-      break;
-    case ST_CONNECT:
-      car.waitConnection();
-      break;
-    case ST_IDLE:
-      car.idleVehicle();
-      break;
-    case ST_MOVE:
-      car.moveVehicle();
-      break;
-    case ST_STOP:
-      car.stopVehicle();
-      break;
+  char charBuf[10];
+  int speedy;
+  char charSpeed[10];
 
-    // Serial.println(state);
-    // int packet = Udp.parsePacket();
-    //
-    // if (packet)
-    // {
-    //   // read the packet into packetBufffer
-    //   int len = Udp.read(car.m_packetBuffer, 255);
-    //   if (len > 0) car.m_packetBuffer[len] = 0;
-    //
-    //   String speedy_str(car.m_packetBuffer);
-    //   car.m_speedy = speedy_str.toInt();
-    //
-    //   Serial.println(car.m_speedy);
-    //   Serial.print(" ");
-    // }
+  /*while (Serial.available() > 0)
+    {
+    Serial.readString().toCharArray(charBuf, 10);
+    sscanf(charBuf, "%s", charSpeed);
+
+    speedy = atof(charSpeed);
+    Serial.print("speedy: ");
+    }*/
+
+  unsigned long startMillis = millis(); // запоминаем текущее время// запоминаем текущее время
+
+  accel.readGXYZ(&ax, &ay, &az); // считываем данные с акселерометра в единицах G
+  gyro.readRadPerSecXYZ(&gx, &gy, &gz); // считываем данные с акселерометра в радианах в секунду
+
+  filter.setKoeff(fps, BETA); // устанавливаем коэффициенты фильтра
+  filter.update(gx, gy, gz, ax, ay, az); // обновляем входные данные в фильтр
+
+  roll = filter.getRollDeg(); // получение углов yaw, pitch и roll из фильтра
+
+  //Serial.print("roll: "); // выводим полученные углы в serial-порт
+  //Serial.println(roll);
+
+  unsigned long deltaMillis = millis() - startMillis; // вычисляем затраченное время на обработку данных
+
+  fps = 1000 / deltaMillis; // вычисляем частоту обработки фильтра
+
+  if (roll < 150.0 && roll > -150.0) // проверяем перевернута ли машинка
+  {
+    reversed = false;
   }
+  else
+  {
+    reversed = true;
+  }
+
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize)
+  {
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    //Serial.println("Contents:");
+    //Serial.println(packetBuffer);
+
+    String str(packetBuffer);
+    String lstr, rstr;
+    str = str.substring(0, str.indexOf(';'));
+    lstr = str.substring(0, str.indexOf(','));
+    rstr = str.substring(str.indexOf(',') + 1, str.indexOf(';'));
+
+    leftSpeed = lstr.toInt();
+    rightSpeed = rstr.toInt();
+
+    Serial.print(leftSpeed);
+    Serial.print(" ");
+    Serial.println(rightSpeed);
+
+    //Udp.flush();
+    // send a reply, to the IP address and port that sent us the packet we received
+    //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    //Udp.write(ReplyBuffer);
+    //Udp.endPacket();
+  }
+
+  moveVehicle(leftSpeed, rightSpeed, reversed);
 }
--- main_prototype.ino
+++ main_prototype.ino
@@ -1,157 +1,124 @@
-#include <Wire.h>
-#include <WiFi.h>
-#include <WiFiUdp.h>
-//#include <TroykaIMU.h>
-#include <WiFiClient.h>
-#include "WEMOS_Motor.h"
+#include <Robot_football.h>
 
-// ------ UDP and AP ------ //
-const char* ssid = "ROBOLEAGUE";
-const char* password = "1234567890";
-
-WiFiUDP Udp;
-unsigned int UdpPort = 4210;                    // local port to listen on
-char packetBuffer[255];      // buffer to hold incoming packet
-char  ReplyBuffer[] = "pososi, potom prosi";    // a string to send backchar
-
-// ------ ---------- ------ //
-
-// STATE MACHINE
-enum state_machine { ST_INIT, ST_WAIT_CONNECT, ST_IDLE, ST_MOVE, ST_STOP };
-
-// множитель фильтра
-#define BETA 0.22f
-
-//Madgwick filter;     // создаём объект для фильтра Madgwick
-//Accelerometer accel; // создаём объект для работы с акселерометром
-//Gyroscope gyro;      // создаём объект для работы с гироскопом
-//
-float gx, gy, gz, ax, ay, az;   // переменные для данных с гироскопов, акселерометров
-float yaw, pitch, roll;         // получаемые углы ориентации
-float fps = 100;                // переменная для хранения частоты выборок фильтра
-bool reversed = false;          // флаг состояния машинки
-int leftSpeed = 0;
-int rightSpeed = 0;
-
-void moveVehicle(int speedy, bool inverted, Motor M1, Motor M2) 
-{
-  // машина состояний, отрабатывающая разные ситуации (перевернутость машинки и направление вектора скорости)
-  if (inverted == false)
-  {
-    M1.setmotor(_CW, speedy);
-    M2.setmotor(_CW, speedy);
-  }
-
-  else if (inverted == true)
-  {
-    M1.setmotor(_CCW, speedy);
-    M2.setmotor(_CCW, speedy);
-  }
-}
-
-void waitConnection()
-{
-  // wait until smb connects to AP
-}
-
-void waitStart()
-{
-  // ST IDLE block code
-}
-
-Motor motorA(0x30, _MOTOR_A, 1000);
+// ---------- Declare all objects
+WiFiUDP Udp;                            // UDP conenction
+Motor motorA(0x30, _MOTOR_A, 1000);     // Motors left and right side
 Motor motorB(0x30, _MOTOR_B, 1000);
-uint8_t state;
+// -------------------- //
 
+// ---------- State machine
+enum state_machine { ST_INIT, ST_CONNECT, ST_IDLE, ST_MOVE, ST_STOP };
+uint8_t state;
+// -------------------- //
+
+// ---------- Class robot
+class Robot
+{
+public:
+  const char* m_ssid = "ROBOLEAGUE";
+  const char* m_password = "1234567890";
+  unsigned int m_UdpPort = 4210;                      // local port to listen on
+  char m_packetBuffer[255];                           // buffer to hold incoming packet
+
+  bool m_carPosition = false;                            // vehicle position
+  int m_speedy = 0;                                   // initial speed
+
+public:
+  void initialize()
+  {
+    state = ST_INIT;
+  }
+
+  void waitConnection()
+  {
+    Serial.println(WiFi.softAP(m_ssid, m_password) ? "AP connected" : "Failed to connect!");
+    Udp.begin(m_UdpPort);
+
+    state = ST_CONNECT;
+  }
+
+  void idleVehicle()
+  {
+    state = ST_IDLE;
+
+    motorA.setmotor(_STANDBY);
+    motorB.setmotor(_STANDBY);
+  }
+
+  void moveVehicle()
+  {
+    state = ST_MOVE;
+
+    if (m_carPosition)
+    {
+      motorA.setmotor(_CW, m_speedy);
+      motorB.setmotor(_CCW, m_speedy);
+    }
+
+    else
+    {
+      motorA.setmotor(_CCW, m_speedy);
+      motorB.setmotor(_CW, m_speedy);
+    }
+  }
+
+  void stopVehicle()
+  {
+    state = ST_STOP;
+
+    motorA.setmotor(_STOP);
+    motorB.setmotor(_STOP);
+  }
+};
+// -------------------- //
+
+Robot car;
 void setup()
 {
-  // открываем последовательный порт
   Serial.begin(115200);
-  Serial.println("Begin init...");
+  Serial.println("Begin initialization ...");
+
   delay(5);
-  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
-  delay(5);
-  Udp.begin(UdpPort);
+  car.waitConnection();
   delay(5);
 
-  // Добавить функции с возвращающими значениями ( if return true -> init sucess )
-//  accel.begin();  // инициализация акселерометра
-//  gyro.begin();   // инициализация гироскопа
   Serial.println("Initialization completed"); // выводим сообщение об удачной инициализации
 }
 
 void loop()
 {
-  while (true)
+  switch (state)
   {
-    switch(state)
-    {
-      case ST_WAIT_CONNECT:
-        waitConnection(); // Ожидания соединения
-        break;
-      case ST_IDLE:
-        waitStart();
-        break;
-      case ST_MOVE:
-        unsigned long startMillis = millis(); // запоминаем текущее время// запоминаем текущее время
-//        accel.readGXYZ(&ax, &ay, &az); // считываем данные с акселерометра в единицах G
-//        gyro.readRadPerSecXYZ(&gx, &gy, &gz); // считываем данные с акселерометра в радианах в секунду
-//
-//        filter.setKoeff(fps, BETA); // устанавливаем коэффициенты фильтра
-//        filter.update(gx, gy, gz, ax, ay, az); // обновляем входные данные в фильтр
+    case ST_INIT:
+      car.initialize();
+      break;
+    case ST_CONNECT:
+      car.waitConnection();
+      break;
+    case ST_IDLE:
+      car.idleVehicle();
+      break;
+    case ST_MOVE:
+      car.moveVehicle();
+      break;
+    case ST_STOP:
+      car.stopVehicle();
+      break;
 
-//        roll = filter.getRollDeg(); // получение углов yaw, pitch и roll из фильтра
-
-        //Serial.print("roll: "); // выводим полученные углы в serial-порт
-        //Serial.println(roll);
-
-        unsigned long deltaMillis = millis() - startMillis; // вычисляем затраченное время на обработку данных
-
-        fps = 1000 / deltaMillis; // вычисляем частоту обработки фильтра
-
-        if (roll < 150.0 && roll > -150.0) // проверяем перевернута ли машинка
-          reversed = false;
-        else
-          reversed = true;
-
-        int packetSize = Udp.parsePacket();
-
-        if (packetSize)
-        {
-          // read the packet into packetBufffer
-          int len = Udp.read(packetBuffer, 255);
-          if (len > 0)
-            packetBuffer[len] = 0;
-          //Serial.println("Contents:");
-          //Serial.println(packetBuffer);
-
-          String str(packetBuffer);
-          String lstr, rstr;
-          str = str.substring(0, str.indexOf(';'));
-          lstr = str.substring(0, str.indexOf(','));
-          rstr = str.substring(str.indexOf(',') + 1, str.indexOf(';'));
-
-          leftSpeed = lstr.toInt();
-          rightSpeed = rstr.toInt();
-
-          Serial.print(leftSpeed);
-          Serial.print(" ");
-          Serial.println(rightSpeed);
-
-          //Udp.flush();
-          // send a reply, to the IP address and port that sent us the packet we received
-          //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-          //Udp.write(ReplyBuffer);
-          //Udp.endPacket();
-        }
-
-        moveVehicle(leftSpeed, reversed, motorA, motorB);
-        
-        break;
-//      case ST_STOP:
-//        Serial.print("Stop");
-//        break;
-    }
+    // Serial.println(state);
+    // int packet = Udp.parsePacket();
+    //
+    // if (packet)
+    // {
+    //   // read the packet into packetBufffer
+    //   int len = Udp.read(car.m_packetBuffer, 255);
+    //   if (len > 0) car.m_packetBuffer[len] = 0;
+    //
+    //   String speedy_str(car.m_packetBuffer);
+    //   car.m_speedy = speedy_str.toInt();
+    //
+    //   Serial.println(car.m_speedy);
+    //   Serial.print(" ");
+    // }
   }
-
 }
